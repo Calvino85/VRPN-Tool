c4f9f5f3549b20d866d68dbee36a2594f210180a
Andres Gomez <and-gome@uniandes.edu.co>
2015-10-29 01:51:12 -0500

    Scripts inspector view personalized. The play and record scripts view in the inspector was modified to facilitate its use. The VRPNEventManager was modified to facilitate the use of the StartListening and StopListening methods. Prefabs were created for the VRPNEventManager and for each device recording and play. Scenes example were created for the three devices. Unnecessary things were removed.

---
+++ VRPNWrapper/Assets/Editor/AwesomeEditorWindow.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AwesomeEditorWindow : EditorWindow
+{
+    [MenuItem("Awesome Stuff/Awesome Window")]
+    public static void ShowAwesomeEditorWindow()
+    {
+        AwesomeEditorWindow window = GetWindow<AwesomeEditorWindow>();
+        window.titleContent.text = "The Window";
+    }
+
+    private Color[] pixels;
+    private int width;
+    private int height;
+    private Texture pixelTexture;
+    private bool isEnabled = false;
+
+    public void OnEnable()
+    {
+        if (!isEnabled)
+        {
+            isEnabled = true;
+
+            width = height = 8;
+            pixels = new Color[width * height];
+
+            for (int i = 0; i < pixels.Length; i++)
+                pixels[i] = new Color(Random.value, Random.value, Random.value, 1.0f);
+
+            pixelTexture = EditorGUIUtility.whiteTexture;
+        }
+        
+    }
+
+    private Color RandomColor()
+    {
+        return new Color(Random.value, Random.value, Random.value, 1.0f);
+    }
+
+    public void OnGUI()
+    {
+        Event evt = Event.current;
+
+        Color oldColor = GUI.color;
+        GUILayout.BeginHorizontal();
+        for (int i = 0; i < width; i++)
+        {
+            GUILayout.BeginVertical();
+            for (int j = 0; j < height; j++)
+            {
+                int index = j + i * height;
+
+                GUI.color = pixels[index];
+                Rect pixelRect = GUILayoutUtility.GetRect(GUIContent.none, GUIStyle.none, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+                GUI.DrawTexture(pixelRect, pixelTexture);
+                if (evt.type == EventType.MouseDown && pixelRect.Contains(evt.mousePosition))
+                {
+                    pixels[index] = RandomColor();
+
+                    evt.Use();
+                }
+            }
+            GUILayout.EndVertical();
+        }
+        GUILayout.EndHorizontal();
+        GUI.color = oldColor;
+    }
+}
---
+++ VRPNWrapper/Assets/Scripts/PruebaMouse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PruebaMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        VRPNEventManager.StartListeningAnalog(VRPNManager.Analog_Types.vrpn_Mouse, VRPNDeviceConfig.Device_Names.Mouse0, moveWithMouse);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void moveWithMouse(string name, VRPNAnalog.AnalogReport report)
+    {
+        this.transform.position = new Vector3((float)report.channel[0]*10 - 5, (1-(float)report.channel[1])*10 - 5, this.transform.position.z);
+    }
+}
